2673ec9d6daaa0de9387c66ba1d3bc523e69575e
Jim Darby <uberscubajim@gmail.com>
2017-09-03 14:56:56 +0100

    Now it's threadable so we can process multiple things at once.

--- Jimbo/Cheerlights/MessageListener.java
+++ Jimbo/Cheerlights/MessageListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Jim Darby.
+ * Copyright (C) 2016, 2017 Jim Darby.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -25,43 +25,71 @@ import java.net.MulticastSocket;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.net.SocketException;
-
 
 /**
  *
  * @author pi
  */
-public class MessageListener
+public class MessageListener implements Runnable
 {
     private static final Logger LOG = Logger.getLogger ("MessageListener");
-    
-    public static void run (CheerListener target) throws UnknownHostException, IOException, InterruptedException
+
+    public MessageListener (CheerListener target)
     {
-        final MulticastSocket socket = new MulticastSocket (5123);
-        final InetAddress group = InetAddress.getByName ("224.1.1.1");
+        LOG.info ("MessageListener created");
+	this.target = target;
+    }
+    
+    public void start ()
+    {
+        Thread t = new Thread (this, "Multicast Listener");
         
-        socket.joinGroup (group);
-        
-        final byte[] buffer = new byte[1024];
-	final DatagramPacket packet = new DatagramPacket (buffer, buffer.length);
-
-	LOG.log (Level.INFO, "And we're off....");
-
-        while (true)
+        t.start ();
+        LOG.info ("MessageListener started");
+    }
+    
+    @Override
+    public void run ()
+    {
+        try
         {
-            socket.receive (packet);
+            final MulticastSocket socket = new MulticastSocket (5123);
+            final InetAddress group = InetAddress.getByName ("224.1.1.1");
 
-            final InetAddress rx_addr = packet.getAddress ();
-            final int rx_port = packet.getPort ();            
-            final byte[] data = new byte[packet.getLength()];
-            
-            System.arraycopy(packet.getData(), 0, data, 0, packet.getLength ());
-            
-            final Message m = new Message (data);
-            
-            LOG.log (Level.INFO, "{0}:{1}: {2}", new Object[] {rx_addr.getCanonicalHostName (), rx_port, m.getText ()});
-            target.update (m.getRGB ());
+            socket.joinGroup (group);
+
+            final byte[] buffer = new byte[1024];
+            final DatagramPacket packet = new DatagramPacket (buffer, buffer.length);
+
+            LOG.log (Level.INFO, "And we're off....");
+
+            while (true)
+            {
+                socket.receive (packet);
+
+                final InetAddress rx_addr = packet.getAddress ();
+                final int rx_port = packet.getPort ();            
+                final byte[] data = new byte[packet.getLength()];
+
+                System.arraycopy(packet.getData(), 0, data, 0, packet.getLength ());
+
+                final Message m = new Message (data);
+
+                LOG.log (Level.INFO, "{0}:{1}: {2}", new Object[] {rx_addr.getCanonicalHostName (), rx_port, m.getText ()});
+                target.update (m.getRGB ());
+            }
+        }
+        
+        catch (UnknownHostException e)
+        {
+            LOG.log (Level.WARNING, "MessageListener failed: UnknownHostException {0}", e.toString ());
+        }
+        
+        catch (IOException  e)
+        {
+            LOG.log (Level.WARNING, "MessageListener failed: IOException {0}", e.toString ());
         }
     }
+
+    private final CheerListener target;
 }
